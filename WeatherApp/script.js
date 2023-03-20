7231d68228c51786f30ab99a8c9cb809b69748dc
andersonguse <andersonguse53469@gmail.com>
2023-03-20 11:48:16 -0400

    updated script.js

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,9 +1,10 @@
 let weather = {
     "apiKey": "9bc19e457c1e3ce6e44b5ea0a1a01147",
-    fetchWeather: function() {
+    fetchWeather: function(city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=9bc19e457c1e3ce6e44b5ea0a1a01147"
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid= " + this.apiKey + ""
             ).then((response) => response.json())
             .then((data) => console.log(data));
     }
+    //displayWeather: function(data){}
 }
