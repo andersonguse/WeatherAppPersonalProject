e476b3336fa96eceb5decadd29ba7fec768a7357
andersonguse <andersonguse53469@gmail.com>
2023-03-28 14:30:28 -0400

    Finished up script.js

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,10 +1,38 @@
 let weather = {
-    "apiKey": "9bc19e457c1e3ce6e44b5ea0a1a01147",
+    //test api link for browser: https://api.openweathermap.org/data/2.5/weather?q=Atlanta&units=imperial&appid=9bc19e457c1e3ce6e44b5ea0a1a01147
+    apiKey: "9bc19e457c1e3ce6e44b5ea0a1a01147",
     fetchWeather: function(city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid= " + this.apiKey + ""
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey + ""
             ).then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " mp/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function(){
+    this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function(){weather.search();});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
     }
-    //displayWeather: function(data){}
-}
+});
+
+weather.fetchWeather("Atlanta");
